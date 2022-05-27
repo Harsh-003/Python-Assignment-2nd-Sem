ef79028982951eb32ff3e8a61b72b3ce2c55a8b6
bt21107069 Harshveer Singh <103431369+Harsh-003@users.noreply.github.com>
2022-05-27 21:14:44 +0530

    Add files via upload

---
+++ assignment 4 output.py
@@ -0,0 +1,34 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/acer/Documents/assignment 4.py ===============
+Q1
+Hey user , enter any student's marks: 69
+B
+Q2
+To check whether a year is a leap year or not 
+Hey User, Enter a year 4210
+ Year is not a leap year 
+Q3
+Question1:8x5=40
+Correct
+Question2:6x10=60
+Correct
+Question3:1x7=7
+Correct
+Question4:7x8=78
+ Incorrect. Correct answer is 56
+Question5:5x2=10
+Correct
+Question6:10x10=100
+Correct
+Question7:7x10=70
+Correct
+Question8:6x2=5
+ Incorrect. Correct answer is 12
+Question9:4x7=66
+ Incorrect. Correct answer is 28
+Question10:4x1=4
+Correct
+Q4
+177
