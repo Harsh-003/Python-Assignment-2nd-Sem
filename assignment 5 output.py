ac7992d3e1e929ec8a05e3fc466eee37682ffaeb
bt21107069 Harshveer Singh <103431369+Harsh-003@users.noreply.github.com>
2022-06-15 20:41:09 +0530

    Add files via upload

---
+++ assignment 5 output.py
@@ -0,0 +1,70 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/acer/Documents/assignment 5.py ===============
+Enter a string(without apostrophes) and it will be reversed:"abc"
+Reversed string: "cba"
+Enter starting range:5
+Enter ending range:20
+Enter a number for which the entered range will be divisible by:5
+5
+10
+15
+20
+Enter length of first side of the triangle:3
+Enter length of second side of the triangle:4
+Enter length of third side of the triangle:5
+Area of the trianle is 6.0
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+Enter number of rows:6
+A 
+B C 
+D E F 
+G H I J 
+K L M N O 
+P Q R S T U 
+Prime numbers in an input range.
+Enter starting of range(>=2):4
+Enter ending of range:11
+5
+7
+11
+77
+154
+231
+308
+385
+462
+Enter integer:3
+Enter integer:5
+Enter integer:6
+Enter integer:9
+Enter integer:77
+Enter integer:67
+Enter integer:8
+Enter integer:9
+Enter integer:18
+a) Positive integers: [3, 5, 6, 9, 77, 67, 8, 9, 18]
+b) Negative integers: []
+c) Odd integers: [3, 5, 9, 77, 67, 9]
+d) Even integers: [6, 8, 18]
+e)
+integer:3 ; number of times it occurs: 1
+integer:5 ; number of times it occurs: 1
+integer:6 ; number of times it occurs: 1
+integer:9 ; number of times it occurs: 2
+integer:77 ; number of times it occurs: 1
+integer:67 ; number of times it occurs: 1
+integer:8 ; number of times it occurs: 1
+integer:18 ; number of times it occurs: 1
+Enter a list of words(strings):("abc" , "def" )
+word:abc ; number of times it occurs:1
+word:def ; number of times it occurs:1
