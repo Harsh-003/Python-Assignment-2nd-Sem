dc716ea25d3df5a92ff008d2e4464758ae87ec9f
bt21107069 Harshveer Singh <103431369+Harsh-003@users.noreply.github.com>
2022-07-08 21:30:53 +0530

    Add files via upload

---
+++ assignment 6 output.py
@@ -0,0 +1,45 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/acer/Documents/assignment 6.py ===============
+Q1
+Enter a number to check if it is perfect:6
+6 is a perfect number
+Q2
+Enter a word, phrase or sequence to see if it is a palindrome:aassas
+It is not a palindrome.
+Q3
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+Q4
+Enter a string to check if it is a pangram: "gafac"
+It is not a pangram.
+Q5
+Enter a hyphen-separated sequence of words: "one-ons"
+"one-ons"
+Q6
+
+Student ID: 21107221
+Student Name: Matthew
+
+Student ID: 21101201
+Student Name: Henry
+
+Student Name: Henry
+Student Class: 12
+Q7
+<__main__.Student object at 0x000001B8EE7EEEF0>
+Harsh
+<__main__.Marks object at 0x000001B8EE7EEE90>
+70
+82
+class Student: <class 'type'> class Marks: <class 'type'>
+Q8
+[[-10, 2, 8], [-7, -3, 10]]
+Q9
+True
+False
+True
